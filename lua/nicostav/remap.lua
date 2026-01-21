93345a8eb5eb43a68f4400e021955b90f39f5363
nicostav <104652866+nicostav@users.noreply.github.com>
2026-01-21 19:36:45 +0100

    Update remap.lua

--- lua/nicostav/remap.lua
+++ lua/nicostav/remap.lua
@@ -1,2 +1,18 @@
 vim.g.mapleader = " "
+
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex, { desc = '[P]roject [V]iew' })
+
+--
+-- GO Lang specific Keybinds
+--
+
+-- If Err Nil Block
+vim.keymap.set("n", "<leader>ge", function()
+  local lines = {
+    "if err != nil {",
+    "\treturn err",
+    "}",
+  }
+  vim.api.nvim_put(lines, "l", true, true)
+
+end, { desc = "Insert Go if err != nil block" }) 
